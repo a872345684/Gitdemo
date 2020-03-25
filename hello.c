b54ee6c79a11fb0d9d83ceada3f300ee712e1b9c
ys980611 <a872345684>
2020-03-25 15:54:52 +0800

    funciton02 dev02

--- hello.c
+++ hello.c
@@ -4,6 +4,7 @@
 #include<stdio.h>
 int main(int arg, char* argv[]){
     printf("inited by SCM.\n");
+sss
     printf("develop branch created by SCM.\n");
-
+printf("develop banch created by dev02dddddd!");
 }
